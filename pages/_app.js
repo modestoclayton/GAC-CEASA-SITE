3b704e65d411709289f4c2bd29e22803707fd119
modestoclayton <modestoclayton@gmail.com>
2026-08-17 15:46:40 -0300

    Atualizar o _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
+import '../gac-colors.css' // Esta linha vai ativar o layout do Tailwind no app inteiro
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
