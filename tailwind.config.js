ee5129cd9b09d6301be2eb011128b83808626f2d
modestoclayton <modestoclayton@gmail.com>
2026-08-17 09:46:11 -0300

    Atualizar o tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./gac-colors.css"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
