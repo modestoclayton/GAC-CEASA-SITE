aaf5045984b05721dc205cfcc725e67de93953a1
modestoclayton <modestoclayton@gmail.com>
2026-08-19 06:19:43 -0300

    Add favicon to _app.js
    Added Head component to include favicon in the app.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/gac-colors.css' // Ajustado para buscar dentro da pasta styles
+import Head from 'next/head';
+import '../styles/gac-colors.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/GAC_CEASA.ico" type="image/x-icon" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
