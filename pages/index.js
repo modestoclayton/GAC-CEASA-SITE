f9b422380417ecbec065d2182f67ff8f71aa4fbd
modestoclayton <modestoclayton@gmail.com>
2026-09-11 16:20:07 -0300

    Usar og-image-square.jpg (banner completo em formato quadrado) como og:image

--- pages/index.js
+++ pages/index.js
@@ -24,10 +24,10 @@ export default function Home() {
           property="og:description"
           content="Gestão completa do seu pátio: compras, vendas, estoque e conta corrente. 30 dias de teste grátis."
         />
-        <meta property="og:image" content="https://gacceasa.com.br/apple-touch-icon.png" />
-        <meta property="og:image:width" content="1254" />
-        <meta property="og:image:height" content="1254" />
-        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image" content="https://gacceasa.com.br/og-image-square.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="1200" />
+        <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:url" content="https://gacceasa.com.br" />
         <meta property="og:type" content="website" />
 
@@ -38,7 +38,7 @@ export default function Home() {
           name="twitter:description"
           content="Gestão completa do seu pátio: compras, vendas, estoque e conta corrente."
         />
-        <meta name="twitter:image" content="https://gacceasa.com.br/apple-touch-icon.png" />
+        <meta name="twitter:image" content="https://gacceasa.com.br/og-image-square.jpg" />
       </Head>
       <GacCeasaApp />
     </>
