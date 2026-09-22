e07545314684ec27f461fb5a1748caacdecf48d0
modestoclayton <modestoclayton@gmail.com>
2026-09-22 12:58:59 -0300

    Atualizar o _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,13 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9437800619610652"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
